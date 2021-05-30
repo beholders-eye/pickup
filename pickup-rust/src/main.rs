68e5c08f4cfeb1447a37ce709b9ed5683aec9f75
Andy O'Neill <andy@potatoriot.com>
2021-05-30 16:48:23 -0400

    [Rust] Initial filesystem walker & cache
    When operating on a slow disk like a network Samba mounted over wifi, it can
    take about 5 minutes to list approximately 50,000 mp3 files. Since our files
    don't often change, we can cache the results of this file scan locally and
    reload it from cache on startup.
    
    Dependencies:
    - We use walkdir to walk through the dir and get all the file listings. It is
      several times faster than just doing it through `fs:read_dir()`.
    - We are using 'clap' for command line arg parsing.
    - We are using "sled" as the DB, with each entry being an integer key mapped to
      an encoded string of the Path.
    - We use bincode to encode strings for sled.
    
    In the future I expect to be loading these files and transforming them into
    an in-memory structure. I think it's sensible to cache them close to their
    raw form as directory listings because we can expect to change the in-memory
    form frequently, so keeping the cache simple seems like a good idea.

--- pickup-rust/src/main.rs
+++ pickup-rust/src/main.rs
@@ -1,20 +1,52 @@
 use actix_web::{App, HttpServer};
-use player::Command;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::thread;
 
 mod api;
 mod app_state;
+mod filemanager;
 mod player;
+
+use clap::{App as ClapApp, Arg};
 
 use app_state::AppState;
 use env_logger::Env;
-use player::Player;
+use filemanager::{load, refresh, MusicDb};
+use player::{Command, Player};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let args = ClapApp::new("pickup-rust")
+        .about("your-app-description")
+        .author("Andy O'Neill")
+        .args(&[
+            Arg::new("refresh")
+                .about("Refresh the music files (can be slow on network drives)")
+                .short('r')
+                .long("refresh"),
+            Arg::new("music_dir")
+                .about("The root music directory")
+                .short('d')
+                .long("music-dir")
+                .takes_value(true),
+        ])
+        .get_matches();
+
+    let music_dir = args
+        .value_of("music_dir")
+        .unwrap_or("../music")
+        .trim_end_matches('/');
+
+    let files: MusicDb;
+    if args.is_present("refresh") {
+        files = refresh(String::from(music_dir)).unwrap();
+    } else {
+        files = load(String::from(music_dir)).unwrap();
+    }
+    log::info!("We have got {} files", files.len());
 
     let sender = spawn_player();
 
